8047ff7c391d0be4f38ebad5562eae733a1c3af6
Rajaesakki1 <rajaesakki1806@gmail.com>
2025-07-14 21:07:33 +0530

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,7 @@ function App() {
   return (
     <div>
       <Navbar/>
-      <Hero/>
-      <About/>
-      <Projects/>
-      <Contact/>
+      
       <Hero/>
       <About/>
       <Projects/>
